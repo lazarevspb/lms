29d18222a204228a3babc3c876309e25246c0f13
Valeriy Lazarev <v.lazarev.spb@gmail.com>
2021-09-10 12:23:58 +0300

    initial commit

---
+++ src/main/java/ru/gbteam/lms/LifeLongLearningApplication.java
@@ -0,0 +1,13 @@
+package ru.gbteam.lms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeLongLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeLongLearningApplication.class, args);
+	}
+
+}
