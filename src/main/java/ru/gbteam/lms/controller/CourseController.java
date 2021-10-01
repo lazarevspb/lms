6b107a1eb0c2e70fa594ee5c32e965b0e8087576
Valeriy Lazarev <v.lazarev.spb@gmail.com>
2021-10-01 18:27:54 +0300

    refactor all

--- src/main/java/ru/gbteam/lms/controller/CourseController.java
+++ src/main/java/ru/gbteam/lms/controller/CourseController.java
@@ -10,15 +10,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import ru.gbteam.lms.exception.NotFoundException;
 import ru.gbteam.lms.model.Course;
 import ru.gbteam.lms.service.CourseService;
+import ru.gbteam.lms.service.ModuleService;
 
 @Controller
 @RequestMapping("/course")
 public class CourseController {
 
     private final CourseService courseService;
+    private final ModuleService moduleService;
 
-    public CourseController(CourseService courseService) {
+    public CourseController(CourseService courseService, ModuleService moduleService) {
         this.courseService = courseService;
+        this.moduleService = moduleService;
     }
 
     @DeleteMapping("/{id}")
@@ -33,7 +36,7 @@ public class CourseController {
         return "course_form";
     }
 
-    @PostMapping
+    @PostMapping("/save")
     public String saveCourse(Course course) {
         courseService.save(course);
         return "redirect:/course";
@@ -50,5 +53,11 @@ public class CourseController {
     public String courseTable(Model model) {
         model.addAttribute("courses", courseService.findAll());
         return "course_table";
+    }
+
+    @GetMapping("{course_id}/module")
+    public String moduleTable(Model model, @PathVariable("course_id") Long course_id) {
+        model.addAttribute("modules", moduleService.findAllByCourseId(course_id));
+        return "module_table";
     }
 }
