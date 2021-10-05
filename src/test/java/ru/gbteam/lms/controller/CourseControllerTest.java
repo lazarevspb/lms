fbaa979a8defcf3dfae4dec5d892db94456c8b23
Valeriy Lazarev <v.lazarev.spb@gmail.com>
2021-10-05 20:27:57 +0300

    fix CourseControllerTest.java

--- src/test/java/ru/gbteam/lms/controller/CourseControllerTest.java
+++ src/test/java/ru/gbteam/lms/controller/CourseControllerTest.java
@@ -1,11 +1,5 @@
 package ru.gbteam.lms.controller;
 
-import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.when;
-
-import java.util.ArrayList;
-import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,145 +10,209 @@ import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilde
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.ui.ConcurrentModel;
 import ru.gbteam.lms.model.Course;
+import ru.gbteam.lms.model.User;
+import ru.gbteam.lms.repository.CourseRepository;
+import ru.gbteam.lms.repository.ModuleRepository;
+import ru.gbteam.lms.repository.UserRepository;
 import ru.gbteam.lms.service.CourseService;
+import ru.gbteam.lms.service.ModuleService;
+import ru.gbteam.lms.service.UserService;
+import ru.gbteam.lms.service.impl.CourseServiceImpl;
+import ru.gbteam.lms.service.impl.ModuleServiceImpl;
+import ru.gbteam.lms.service.impl.UserServiceImpl;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 @ContextConfiguration(classes = {CourseController.class})
 @ExtendWith(SpringExtension.class)
 class CourseControllerTest {
+    @Autowired
+    private CourseController courseController;
 
-  @Autowired
-  private CourseController courseController;
+    @MockBean
+    private CourseService courseService;
 
-  @MockBean
-  private CourseService courseService;
+    @MockBean
+    private ModuleService moduleService;
 
-  @Test
-  void testCourseForm() throws Exception {
-    MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/course/new");
-    MockMvcBuilders.standaloneSetup(this.courseController)
-        .build()
-        .perform(requestBuilder)
-        .andExpect(MockMvcResultMatchers.status().isOk())
-        .andExpect(MockMvcResultMatchers.model().size(1))
-        .andExpect(MockMvcResultMatchers.model().attributeExists("course"))
-        .andExpect(MockMvcResultMatchers.view().name("course_form"))
-        .andExpect(MockMvcResultMatchers.forwardedUrl("course_form"));
-  }
+    @MockBean
+    private UserService userService;
 
-  @Test
-  void testCourseForm2() throws Exception {
-    MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/course/new",
-        "Uri Vars");
-    MockMvcBuilders.standaloneSetup(this.courseController)
-        .build()
-        .perform(requestBuilder)
-        .andExpect(MockMvcResultMatchers.status().isOk())
-        .andExpect(MockMvcResultMatchers.model().size(1))
-        .andExpect(MockMvcResultMatchers.model().attributeExists("course"))
-        .andExpect(MockMvcResultMatchers.view().name("course_form"))
-        .andExpect(MockMvcResultMatchers.forwardedUrl("course_form"));
-  }
+    @Test
+    void testAssignUser() throws Exception {
+        User user = new User();
+        user.setCourses(new HashSet<>());
+        Optional<User> ofResult = Optional.of(user);
+        when(userService.findById(123L)).thenReturn(ofResult);
 
-  @Test
-  void testCourseForm3() throws Exception {
-    when(this.courseService.findById(any())).thenReturn(Optional.of(new Course()));
-    MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/course/{id}", 123L);
-    MockMvcBuilders.standaloneSetup(this.courseController)
-        .build()
-        .perform(requestBuilder)
-        .andExpect(MockMvcResultMatchers.status().isOk())
-        .andExpect(MockMvcResultMatchers.model().size(1))
-        .andExpect(MockMvcResultMatchers.model().attributeExists("course"))
-        .andExpect(MockMvcResultMatchers.view().name("course_form"))
-        .andExpect(MockMvcResultMatchers.forwardedUrl("course_form"));
-  }
+        Course course = new Course();
+        course.setUsers(new HashSet<>());
+        Optional<Course> ofResult1 = Optional.of(course);
+        when(courseService.findById(123L)).thenReturn(ofResult1);
+        MockHttpServletRequestBuilder postResult = MockMvcRequestBuilders
+                .post("/course/{courseId}/assign", 123L);
+        MockHttpServletRequestBuilder requestBuilder = postResult.param("userId", String.valueOf(123L));
+        MockMvcBuilders.standaloneSetup(courseController)
+                .build()
+                .perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isFound())
+                .andExpect(MockMvcResultMatchers.model().size(0))
+                .andExpect(MockMvcResultMatchers.view().name("redirect:/course"))
+                .andExpect(MockMvcResultMatchers.redirectedUrl("/course"));
+    }
 
-  @Test
-  void testCourseTable() throws Exception {
-    when(this.courseService.findAll()).thenReturn(new ArrayList<>());
-    MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/course");
-    MockMvcBuilders.standaloneSetup(this.courseController)
-        .build()
-        .perform(requestBuilder)
-        .andExpect(MockMvcResultMatchers.status().isOk())
-        .andExpect(MockMvcResultMatchers.model().size(1))
-        .andExpect(MockMvcResultMatchers.model().attributeExists("courses"))
-        .andExpect(MockMvcResultMatchers.view().name("course_table"))
-        .andExpect(MockMvcResultMatchers.forwardedUrl("course_table"));
-  }
+    @Test
+    void testAssignCourse() {
+        UserRepository userRepository = mock(UserRepository.class);
+        when(userRepository.findAll()).thenReturn(new ArrayList<>());
+        UserServiceImpl userService = new UserServiceImpl(userRepository);
+        CourseServiceImpl courseService = new CourseServiceImpl(mock(CourseRepository.class));
+        CourseController courseController = new CourseController(courseService,
+                new ModuleServiceImpl(mock(ModuleRepository.class)), userService);
+        assertEquals("assign", courseController.assignCourse(new ConcurrentModel(), "1"));
+        verify(userRepository).findAll();
+    }
 
-  @Test
-  void testCourseTable2() throws Exception {
-    when(this.courseService.findAll()).thenReturn(new ArrayList<>());
-    MockHttpServletRequestBuilder getResult = MockMvcRequestBuilders.get("/course");
-    getResult.contentType("Not all who wander are lost");
-    MockMvcBuilders.standaloneSetup(this.courseController)
-        .build()
-        .perform(getResult)
-        .andExpect(MockMvcResultMatchers.status().isOk())
-        .andExpect(MockMvcResultMatchers.model().size(1))
-        .andExpect(MockMvcResultMatchers.model().attributeExists("courses"))
-        .andExpect(MockMvcResultMatchers.view().name("course_table"))
-        .andExpect(MockMvcResultMatchers.forwardedUrl("course_table"));
-  }
+    @Test
+    void testCourseForm() throws Exception {
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/course/new");
+        MockMvcBuilders.standaloneSetup(courseController)
+                .build()
+                .perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.model().size(1))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("course"))
+                .andExpect(MockMvcResultMatchers.view().name("course_form"))
+                .andExpect(MockMvcResultMatchers.forwardedUrl("course_form"));
+    }
 
-  @Test
-  void testDeleteCourse() throws Exception {
-    doNothing().when(this.courseService).delete(any());
-    MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.delete("/course/{id}",
-        123L);
-    MockMvcBuilders.standaloneSetup(this.courseController)
-        .build()
-        .perform(requestBuilder)
-        .andExpect(MockMvcResultMatchers.status().isFound())
-        .andExpect(MockMvcResultMatchers.model().size(0))
-        .andExpect(MockMvcResultMatchers.view().name("redirect:/course"))
-        .andExpect(MockMvcResultMatchers.redirectedUrl("/course"));
-  }
+    @Test
+    void testCourseForm2() throws Exception {
+        MockHttpServletRequestBuilder getResult = MockMvcRequestBuilders.get("/course/new");
+        MockMvcBuilders.standaloneSetup(courseController)
+                .build()
+                .perform(getResult)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.model().size(1))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("course"))
+                .andExpect(MockMvcResultMatchers.view().name("course_form"))
+                .andExpect(MockMvcResultMatchers.forwardedUrl("course_form"));
+    }
 
-  @Test
-  void testDeleteCourse2() throws Exception {
-    doNothing().when(this.courseService).delete(any());
-    MockHttpServletRequestBuilder deleteResult = MockMvcRequestBuilders.delete("/course/{id}",
-        123L);
-    deleteResult.contentType("Not all who wander are lost");
-    MockMvcBuilders.standaloneSetup(this.courseController)
-        .build()
-        .perform(deleteResult)
-        .andExpect(MockMvcResultMatchers.status().isFound())
-        .andExpect(MockMvcResultMatchers.model().size(0))
-        .andExpect(MockMvcResultMatchers.view().name("redirect:/course"))
-        .andExpect(MockMvcResultMatchers.redirectedUrl("/course"));
-  }
+    @Test
+    void testCourseForm3() throws Exception {
+        when(moduleService.findAllByCourseId(123L)).thenReturn(new ArrayList<>());
 
-  @Test
-  void testSaveCourse() throws Exception {
-    when(this.courseService.findAll()).thenReturn(new ArrayList<>());
-    MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/course");
-    MockMvcBuilders.standaloneSetup(this.courseController)
-        .build()
-        .perform(requestBuilder)
-        .andExpect(MockMvcResultMatchers.status().isOk())
-        .andExpect(MockMvcResultMatchers.model().size(1))
-        .andExpect(MockMvcResultMatchers.model().attributeExists("courses"))
-        .andExpect(MockMvcResultMatchers.view().name("course_table"))
-        .andExpect(MockMvcResultMatchers.forwardedUrl("course_table"));
-  }
+        Course course = new Course();
+        course.setUsers(new HashSet<>());
 
-  @Test
-  void testSaveCourse2() throws Exception {
-    when(this.courseService.findAll()).thenReturn(new ArrayList<>());
-    MockHttpServletRequestBuilder getResult = MockMvcRequestBuilders.get("/course");
-    getResult.contentType("Not all who wander are lost");
-    MockMvcBuilders.standaloneSetup(this.courseController)
-        .build()
-        .perform(getResult)
-        .andExpect(MockMvcResultMatchers.status().isOk())
-        .andExpect(MockMvcResultMatchers.model().size(1))
-        .andExpect(MockMvcResultMatchers.model().attributeExists("courses"))
-        .andExpect(MockMvcResultMatchers.view().name("course_table"))
-        .andExpect(MockMvcResultMatchers.forwardedUrl("course_table"));
-  }
+        Optional<Course> ofResult = Optional.of(course);
+        when(courseService.findById(any())).thenReturn(ofResult);
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/course/{id}", 123L);
+        MockMvcBuilders.standaloneSetup(courseController)
+                .build()
+                .perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.model().size(3))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("course", "modules", "users"))
+                .andExpect(MockMvcResultMatchers.view().name("course_form"))
+                .andExpect(MockMvcResultMatchers.forwardedUrl("course_form"));
+    }
+
+    @Test
+    void testCourseTable() throws Exception {
+        when(courseService.findAll()).thenReturn(new ArrayList<>());
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/course");
+        MockMvcBuilders.standaloneSetup(courseController)
+                .build()
+                .perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.model().size(1))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("courses"))
+                .andExpect(MockMvcResultMatchers.view().name("course_table"))
+                .andExpect(MockMvcResultMatchers.forwardedUrl("course_table"));
+    }
+
+    @Test
+    void testCourseTable2() throws Exception {
+        when(courseService.findAll()).thenReturn(new ArrayList<>());
+        MockHttpServletRequestBuilder getResult = MockMvcRequestBuilders.get("/course");
+        MockMvcBuilders.standaloneSetup(courseController)
+                .build()
+                .perform(getResult)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.model().size(1))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("courses"))
+                .andExpect(MockMvcResultMatchers.view().name("course_table"))
+                .andExpect(MockMvcResultMatchers.forwardedUrl("course_table"));
+    }
+
+    @Test
+    void testDeleteCourse() throws Exception {
+        doNothing().when(courseService).delete(123L);
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.delete("/course/{id}", 123L);
+        MockMvcBuilders.standaloneSetup(courseController)
+                .build()
+                .perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isFound())
+                .andExpect(MockMvcResultMatchers.model().size(0))
+                .andExpect(MockMvcResultMatchers.view().name("redirect:/course"))
+                .andExpect(MockMvcResultMatchers.redirectedUrl("/course"));
+    }
+
+    @Test
+    void testSaveCourse() throws Exception {
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.post("/course/save");
+        MockMvcBuilders.standaloneSetup(courseController)
+                .build()
+                .perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isFound())
+                .andExpect(MockMvcResultMatchers.model().size(1))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("course"))
+                .andExpect(MockMvcResultMatchers.view().name("redirect:/course"))
+                .andExpect(MockMvcResultMatchers.redirectedUrl("/course"));
+    }
+
+    @Test
+    void testSaveCourse2() throws Exception {
+        MockHttpServletRequestBuilder postResult = MockMvcRequestBuilders.post("/course/save");
+        MockMvcBuilders.standaloneSetup(courseController)
+                .build()
+                .perform(postResult)
+                .andExpect(MockMvcResultMatchers.status().isFound())
+                .andExpect(MockMvcResultMatchers.model().size(1))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("course"))
+                .andExpect(MockMvcResultMatchers.view().name("redirect:/course"))
+                .andExpect(MockMvcResultMatchers.redirectedUrl("/course"));
+    }
+
+    @Test
+    void testUnAssignUser() throws Exception {
+        User user = new User();
+        user.setCourses(new HashSet<>());
+        Optional<User> ofResult = Optional.of(user);
+        when(userService.findById(100L)).thenReturn(ofResult);
+
+        Course course = new Course();
+        course.setUsers(new HashSet<>());
+        Optional<Course> ofResult1 = Optional.of(course);
+        when(courseService.findById(123L)).thenReturn(ofResult1);
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.delete("/course/{courseId}/unassign/{userId}",
+                123L, 100L);
+        MockMvcBuilders.standaloneSetup(courseController)
+                .build()
+                .perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isFound())
+                .andExpect(MockMvcResultMatchers.model().size(0))
+                .andExpect(MockMvcResultMatchers.view().name("redirect:/course/123"))
+                .andExpect(MockMvcResultMatchers.redirectedUrl("/course/123"));
+    }
 }
 
