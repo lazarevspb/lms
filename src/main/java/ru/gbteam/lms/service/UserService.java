1f1a5c865d85f7c255e1b398bbd9ff7b34646f1f
Valeriy Lazarev <v.lazarev.spb@gmail.com>
2021-10-06 15:14:00 +0300

    Added BD authorization

--- src/main/java/ru/gbteam/lms/service/UserService.java
+++ src/main/java/ru/gbteam/lms/service/UserService.java
@@ -6,6 +6,9 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserService {
+
+    Optional<User> findUserByUsername(String username);
+
     List<User> findAll();
 
     Optional<User> findById(Long id);
